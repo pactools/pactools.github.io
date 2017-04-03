3eb0361f882e008c24dd6fc9f994664dd1496182
Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>
2017-04-03 19:25:27 +0200

    Make install

--- _downloads/plot_dar_model.py
+++ _downloads/plot_dar_model.py
@@ -1,6 +1,6 @@
 """
-Example of fitting a DAR model
-------------------------------
+Fitting a DAR model
+-------------------
 This example creates an artificial signal with phase-amplitude coupling (PAC),
 fits a DAR model and show the modulation extracted in the DAR model.
 
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from pactools import Comodulogram
-from pactools import create_signal
+from pactools import simulate_pac
 from pactools.dar_model import DAR, extract_driver
 
 fs = 200.  # Hz
@@ -23,22 +23,27 @@ noise_level = 0.4
 t_plot = 2.0  # sec
 
 # Create the artificial signal with PAC
-signal = create_signal(n_points=n_points, fs=fs, high_fq=high_fq,
-                       low_fq=low_fq, low_fq_width=low_fq_width,
-                       noise_level=noise_level, random_state=0)
+signal = simulate_pac(n_points=n_points, fs=fs, high_fq=high_fq, low_fq=low_fq,
+                      low_fq_width=low_fq_width, noise_level=noise_level,
+                      random_state=0)
 
-# Prepare the plot for the two figures
+###############################################################################
+# Prepare the plot for the three figures
 fig, axs = plt.subplots(1, 3, figsize=(14, 4))
 axs = axs.ravel()
 
-# Extract a low frequency band and fit a DAR model
+# Extract a low frequency band
 sigdriv, sigin, sigdriv_imag = extract_driver(
     sigs=signal, fs=fs, low_fq=low_fq, bandwidth=low_fq_width,
     extract_complex=True, random_state=0, fill=2)
 
+# Create a DAR model
 # Here we use BIC selection to get optimal hyperparameters (ordar, ordriv)
 dar = DAR(ordar=20, ordriv=2, criterion='bic')
+# Fit the DAR model
 dar.fit(sigin=sigin, sigdriv=sigdriv, sigdriv_imag=sigdriv_imag, fs=fs)
+
+# Plot the BIC selection
 bic_array = dar.model_selection_criterions_['bic']
 lines = axs[0].plot(bic_array)
 axs[0].legend(lines, ['ordriv=%d' % d for d in [0, 1, 2]])
@@ -47,12 +52,23 @@ axs[0].set_ylabel('BIC / T')
 axs[0].set_title('BIC order selection')
 axs[0].plot(dar.ordar_, bic_array[dar.ordar_, dar.ordriv_], 'ro')
 
-# Plot the modulation extracted by the optimal model
+# Plot the modulation extracted by the optimal model
 dar.plot(ax=axs[1])
 axs[1].set_title(dar.get_title(name=True))
 
-# Compute the comodulogram: It handles the driver extraction for a range
-# of low frequency, fit a model, and quantify PAC accross the spectrum.
+###############################################################################
+# To compute a comodulogram, we need to perform the same steps for each
+# low frequency: extract the low frequency, fit a DAR model, potentially with
+# a model selection with the BIC, and quantify the PAC accross the spectrum.
+#
+# Everything is handled by the function ``Comodulogram``, by giving a
+# (non-fitted) DAR model in the parameter ``method``.
+# Giving ``method='duprelatour'`` will default to
+# ``DAR(ordar=10, ordriv=1, criterion=None)``, without BIC selection.
+
+# Here we give a different set of parameter, and the BIC selection will be
+# performed independantly for each model (i.e. at each low frequency).
+dar = DAR(ordar=20, ordriv=2, criterion='bic')
 low_fq_range = np.linspace(1, 10, 50)
 estimator = Comodulogram(fs=fs, low_fq_range=low_fq_range,
                          low_fq_width=low_fq_width, method=dar,
